67f49495c0cbc32fe3bd046327c4fbae8537679a
jia <1721009302@qq.com>
2020-04-07 16:12:28 +0800

    线程中的生产者和消费者

--- src/com/atguigu/demo01/MyThreadDemo02.java
+++ src/com/atguigu/demo01/MyThreadDemo02.java
@@ -1,22 +1,46 @@
 package com.atguigu.demo01;
 
-import java.util.concurrent.CopyOnWriteArrayList;
-
 class Shop{
     private int number=0;
-    public void increment() throws  Exception{
+    public synchronized void increment() throws  Exception{
         if (number!=0){
             this.wait();
         }
-        number--;
+        number++;
+        System.out.println(Thread.currentThread().getName()+"\t"+number);
+        this.notifyAll();
     }
-    public void decrement() throws  Exception{
-
+    public  synchronized void decrement() throws  Exception{
+        if (number==0){
+            this.wait();
+        }
+        number--;
+        System.out.println(Thread.currentThread().getName()+"\t"+number);
+        //通知
+        this.notifyAll();
     }
 }
 
 public class MyThreadDemo02 {
     public static void main(String[] args) {
-
+        Shop s=new Shop();
+        new Thread(()->{
+            for (int i = 1; i <33; i++) {
+                try {
+                    s.increment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"线程1").start();
+        new Thread(()->{
+            for (int i = 1; i <33; i++) {
+                try {
+                    s.decrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"线程2").start();
     }
 }
